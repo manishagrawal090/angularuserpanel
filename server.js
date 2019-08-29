d75b06ca64fd3b2f1b43b34e91aff94f154ed767
Manish Agrawal <rohit.kalavadiya@agileinfoways.com>
2019-08-29 17:11:40 +0530

    XZx

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/user-panel-angular'));
 app.listen(process.env.PORT || 8080);
 app.get('/*', function(req,res) {
     
